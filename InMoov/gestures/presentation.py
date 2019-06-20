c189f063481b1541897d07e0ac7ecba8d15b85fe
Alex <alexandre.laruegmail.com>
2019-06-20 18:17:05 +0200

    Reglages bras et epaules Quelques gestures ecrits par chino

---
+++ InMoov/gestures/presentation.py
@@ -0,0 +1,19 @@
+﻿# -- coding: utf-8 --
+
+def presentation():
+  relax()
+  i01.startedGest
+  sleep(1)
+  i01.moveArm("left",53,0,60,10)
+  sleep(2)
+  i01.moveArm("left",53,50,60,10)
+  sleep(2)
+  i01.moveArm("left",53,0,60,10)
+  sleep(2)
+  i01.moveArm("left",53,100,60,10)
+  sleep(0.2)
+  i01.finishedGesture()
+  relax()
+
+
+
